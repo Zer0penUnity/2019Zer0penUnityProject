64aa2d2b72cd43edb710220f0742bb2d5a4b2cb1
GolmokDaeBBang <bluedulgi1104@gmail.com>
2019-07-10 15:53:40 +0900

    Add files via upload

---
+++ Assets/Script/CamController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour {
+
+    public Transform TargetTr;
+    public Vector3 CamPos = new Vector3(0, 2, -3);
+
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        tr.position = Vector3.Lerp(tr.position, TargetTr.position + CamPos, 1f);
+        tr.LookAt(TargetTr);
+	}
+}
---
+++ Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    public float MoveSpeed = 14f;
+    public float JumpPower = 28f;
+    public float RotSpeed = 8f;
+    public int JumpCount = 2;
+
+    private Transform tr;
+    private Rigidbody rb;
+
+    private float horizontal, vertical;
+    private float mouseX;
+
+    private Vector3 moveDir;
+
+    private int jumpcount;
+
+    private bool isGround = false;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.CompareTag("Ground"))
+        {
+            isGround = true;
+            jumpcount = JumpCount;
+        }
+    }
+
+	void Start () {
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxisRaw("Mouse X");
+
+        moveDir = new Vector3(horizontal, 0, vertical);
+
+        if(Input.GetKeyDown(KeyCode.Space) && jumpcount > 0)
+        {
+            rb.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            isGround = false;
+            jumpcount--;
+        }
+
+        tr.Rotate(Vector3.up * mouseX * RotSpeed * Time.deltaTime);
+        tr.Translate(moveDir.normalized * MoveSpeed * Time.deltaTime);
+	}
+}
